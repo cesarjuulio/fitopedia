c0c8bc3f8a4163af412ea965e6f953d181444190
Júlio César <cesarjulio7359@gmail.com>
2025-09-04 15:52:28 -0300

    Inicial 2
    Melhorado tela inicial, adicionado tela de explorar

--- app/src/main/java/br/com/example/fitopedia/MainActivity.java
+++ app/src/main/java/br/com/example/fitopedia/MainActivity.java
@@ -1,6 +1,8 @@
 package br.com.example.fitopedia;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.button.MaterialButton;
+
 public class MainActivity extends AppCompatActivity {
+    private MaterialButton explorarPlantasButton;
+    private MaterialButton iaButton;
+    private MaterialButton sobreButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,32 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        explorarPlantasButton = findViewById(R.id.explorarPlantasButton);
+        iaButton = findViewById(R.id.iaButton);
+        sobreButton = findViewById(R.id.sobreButton);
+
+        explorarPlantasButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ExplorarPlantas.class);
+                startActivity(intent);
+            }
+        });
+        iaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ExplorarPlantas.class);
+                startActivity(intent);
+            }
+        });
+        sobreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ExplorarPlantas.class);
+                startActivity(intent);
+            }
         });
     }
 }
